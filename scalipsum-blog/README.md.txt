e9fdd9fe8742ff4cbf05ecda5161e60320b85d6b
faisalprady13 <faisalprady@gmail.com>
2020-04-16 18:02:34 +0200

    base server created

---
+++ scalipsum-blog/README.md.txt
@@ -0,0 +1,4 @@
+Basic practice from instagram:
+
+- https://www.instagram.com/p/B-9rIm5gJdl/
+- https://www.instagram.com/p/B_APG2cAD3b/
---
+++ scalipsum-blog/Server/routes/posts.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const PostsController = require("../controllers/PostController");
+
+// Middleware: /api/posts
+
+module.exports = router;
